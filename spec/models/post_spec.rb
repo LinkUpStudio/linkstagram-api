55dff39a3c320c687bb5729a51053b0d7de889d7
natalia-vvv <natalia.veselska13@gmail.com>
2021-02-12 17:18:30 +0200

    Adding tests to post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#liked_by?' do
+    let(:post) { create(:post) }
+    let(:user) { create(:account) }
+    let(:other_user) { create(:account) }
+    let!(:like) { create(:like, account: user, post: post) }
+    it 'returns true if user has liked post' do
+      expect(post.liked_by?(user)).to eq(true)
+      expect(post.liked_by?(other_user)).to eq(false)
+    end
+  end
+
+  describe '#find_like_by' do
+    let(:post) { create(:post) }
+    let(:user) { create(:account) }
+    let(:other_user) { create(:account) }
+    let!(:like) { create(:like, account: user, post: post) }
+    it 'returns true if user has liked post' do
+      expect(post.find_like_by(user)).to eq(like)
+      expect(post.find_like_by(other_user)).to eq(nil)
+    end
+  end
 end
