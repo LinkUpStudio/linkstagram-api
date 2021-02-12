be86cdef7a36bd0cff5c67232fd52c92ca594d0a
natalia-vvv <natalia.veselska13@gmail.com>
2021-02-12 09:55:54 +0200

    Fixing likes tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe Like, type: :model do
   describe '.create' do
     let(:post) { create(:post) }
     let(:account) { create(:account) }
-    let(:like) { create(:like, account: account, post: post) }
+    let!(:like) { create(:like, account: account, post: post) }
     it 'does not create second like for the same post' do
       second_like = build(:like, account: account, post: post)
-      p second_like.valid?
       expect(second_like).to_not be_valid
     end
   end
