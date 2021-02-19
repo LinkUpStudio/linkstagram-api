989158eb1da71c6d61b41c019c057295c1935271
Nazar Matus <funkyloverone@gmail.com>
2021-02-19 02:47:43 +0200

    Bundle update & remove ActionDispatch::Flash

--- config/application.rb
+++ config/application.rb
@@ -26,9 +26,6 @@ module LinkstagramApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # FIXME: this is currently needed for rodauth, we need to remove it later
-    config.middleware.use ActionDispatch::Flash
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
