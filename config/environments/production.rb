1e5a767448cbfbd088c990a33952bc8319cc715d
natalia-vvv <natalia.veselska13@gmail.com>
2021-02-19 19:00:44 +0200

    Remove precompile from production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.precompile += %w( apitome/*.css apitome/*.js )
+
   config.assets.compile = true
 
   # Code is not reloaded between requests.
