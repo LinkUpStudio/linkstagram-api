3e1867b2f9d0aaa984ee71224b25db1ebe3766ca
Nazar Matus <funkyloverone@gmail.com>
2021-02-22 18:41:02 +0200

    Monkey-patch RspecApiDocumentation not in initializer

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -1,15 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'rspec_api_documentation'
 require 'rspec_api_documentation/dsl'
 require 'support/helpers'
 require 'support/database_cleaner'
 
+# FIXME: temporary monkey-patched solution, more details here:
+#   https://github.com/zipmark/rspec_api_documentation/issues/456
+module RspecApiDocumentation
+  class RackTestClient < ClientBase
+    def response_body
+      last_response.body.encode('utf-8')
+    end
+  end
+end
+
 RspecApiDocumentation.configure do |config|
   config.format = [:html, :json]
   config.request_body_formatter = proc do |params|
     JSON.pretty_generate(params)
   end
-  config.response_body_formatter = Proc.new do |response_content_type, response_body|
+  config.response_body_formatter = proc do |response_content_type, response_body|
     JSON.pretty_generate(response_content_type)
     response_body
   end
@@ -17,5 +29,5 @@ RspecApiDocumentation.configure do |config|
   config.response_headers_to_include = %w[Content-Type]
 
   config.curl_host = 'http://localhost:3000'
-  config.api_name = "Linkstagram API"
+  config.api_name = 'Linkstagram API'
 end
